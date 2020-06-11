3428711430430596281edfd329597fb1be204c9a
chandu <boosachandu@gmail.com>
2020-06-11 07:27:09 +0530

    added project

---
+++ src/main/java/com/kafka/springbootkafkaconsumerexample/config/AppProperties.java
@@ -0,0 +1,10 @@
+package com.kafka.springbootkafkaconsumerexample.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("connections")
+public class AppProperties {
+
+}
